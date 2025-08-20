70fd5886431f5b99b2109e0c2b048c54b6e7c94a
Benjamin Kao <127792860+matsusakagyu@users.noreply.github.com>
2025-08-19 21:27:14 -0400

    Reorganization

--- MMD/build_mmd.m
+++ MMD/build_mmd.m
@@ -11,7 +11,7 @@ for ib = 1:nb
     for jd = 1:nd
         delta = delta_vec(jd);
         % solve this operating point
-        [Ay_ij, N_ij] = solve_point(p, beta, delta, kappa_f, kappa_r);
+        [Ay_ij, N_ij] = solve_point_2(p, beta, delta, kappa_f, kappa_r);
         Ay(ib,jd) = Ay_ij;
         N(ib,jd)  = N_ij;
         CN(ib,jd) = N_ij / (p.m * p.g * (p.lf + p.lr));  % Eq. (1) in the paper
